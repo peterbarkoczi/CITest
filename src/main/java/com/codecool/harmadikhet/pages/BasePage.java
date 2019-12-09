2a1cf04737a8c28f075648f480ffffa32c74dc2f
adamzopcsak <adamzopcsak@gmail.com>
2019-12-09 11:31:22 +0100

    BasePage.java implemented

--- src/main/java/com/codecool/harmadikhet/pages/BasePage.java
+++ src/main/java/com/codecool/harmadikhet/pages/BasePage.java
@@ -1,4 +1,17 @@
 package com.codecool.harmadikhet.pages;
 
-public class BasePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+public abstract class BasePage {
+    protected WebDriver driver;
+    protected int timeOutInSeconds = 20;
+
+    public BasePage(){}
+
+    BasePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver, timeOutInSeconds), this);
+    }
 }
