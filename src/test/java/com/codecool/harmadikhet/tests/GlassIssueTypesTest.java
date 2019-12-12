770b033516d22cf12a1b19359b8814843c73415f
restarmilan <restarmilan@gmail.com>
2019-12-12 12:53:31 +0100

    add test case for compare issue types on both pages

--- src/test/java/com/codecool/harmadikhet/tests/GlassIssueTypesTest.java
+++ src/test/java/com/codecool/harmadikhet/tests/GlassIssueTypesTest.java
@@ -2,15 +2,19 @@ package com.codecool.harmadikhet.tests;
 
 import com.codecool.harmadikhet.pages.GlassDocumentationPage;
 import com.codecool.harmadikhet.pages.LogInPage;
+import com.codecool.harmadikhet.pages.SystemSettingsPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class GlassIssueTypesTest extends BaseTest {
 
     private LogInPage logInPage;
     private GlassDocumentationPage glassDocumentationPage;
+    private SystemSettingsPage systemSettingsPage;
 
     @BeforeEach
     public void setUp() {
@@ -19,13 +23,16 @@ public class GlassIssueTypesTest extends BaseTest {
     }
 
     @Test
-    public void letsSee() {
+    public void testCompareIssueTypes() {
         glassDocumentationPage = new GlassDocumentationPage(driver);
-        List<String> elements = glassDocumentationPage.getIssueTypes();
-        for (String element : elements) {
-            System.out.println(element);
-        }
+        systemSettingsPage = new SystemSettingsPage(driver);
+        List<String> issueTypesGlass = glassDocumentationPage.getIssueTypes();
+        List<String> issueTypesSystemSettings = systemSettingsPage.getIssueTypesSystemSettings();
+        assertTrue(matcher(issueTypesGlass, issueTypesSystemSettings));
+    }
 
+    public boolean matcher(List<String> compare, List<String> compared) {
+        return compare.equals(compared);
     }
 
 }
