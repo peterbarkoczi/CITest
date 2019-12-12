7544b970b58a0e2df250b7100acfc97a9fc01cfd
restarmilan <restarmilan@gmail.com>
2019-12-12 20:12:14 +0100

    add independent test cases for check created/deleted version

--- src/test/java/com/codecool/harmadikhet/tests/GlassVersionsTest.java
+++ src/test/java/com/codecool/harmadikhet/tests/GlassVersionsTest.java
@@ -6,22 +6,41 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+
 public class GlassVersionsTest extends BaseTest {
 
     private LogInPage logInPage;
     private ProjectSettingsVersionsPage projectSettingsVersionsPage;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         logInPage = new LogInPage(driver);
         logInPage.logIn(username, password);
     }
 
     @ParameterizedTest
     @CsvSource({"4.1, test version"})
-    public void testCreateNewVersion(String name, String description) {
+    void testCreateNewVersion(String name, String description) {
         projectSettingsVersionsPage = new ProjectSettingsVersionsPage(driver);
         projectSettingsVersionsPage.createNewVersion(name, description);
+        assertTrue(projectSettingsVersionsPage.checkNewVersionInProjectSettings(name, description));
+        projectSettingsVersionsPage.deleteNewVersion();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"4.1, test version"})
+    void testDeleteNewVersion(String name, String description) {
+        projectSettingsVersionsPage = new ProjectSettingsVersionsPage(driver);
+        projectSettingsVersionsPage.createNewVersion(name, description);
+        projectSettingsVersionsPage.deleteNewVersion();
+        assertFalse(projectSettingsVersionsPage.checkNewVersionInProjectSettings(name, description));
 
     }
+
+
 }
+
